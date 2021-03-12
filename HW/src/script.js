28b4110f43a4dd82bc50de005debc13065b0b427
Oleg Vokh <olegvokh@gmail.com>
2021-03-12 11:19:42 +0200

    HW 2

--- HW/src/script.js
+++ HW/src/script.js
@@ -1,17 +1,27 @@
 "use strict";
 
 const numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
-const lastWatchedFilm = prompt('Последний просмотреный фильм?');
-const howRate = prompt('Насколько оцените его?');
 
 const personalMovieDB = {
     count: +numberOfFilms,
-    movies: {
-        lastWatchedFilm,
-        howRate
-    },
+    movies: {},
     actors: {},
     genres: {},
     privat: false
 };
-console.log(personalMovieDB);
+for(let i = 0; i < 2; i++){
+    const lastFilm = prompt('Последний просмотреный фильм?'),
+        howRate = prompt('Насколько оцените его?');
+        if(lastFilm != null && howRate != null && lastFilm != '' && howRate != '' && lastFilm.length < 50){
+            personalMovieDB.movies[lastFilm] = howRate;
+        }else{
+            i--;
+        }
+}
+if (numberOfFilms < 10){
+    console.log("Просмотрено довольно мало фильмов");
+}else if (10 < numberOfFilms < 30){
+    console.log("Вы классический зритель");
+}else if (30 < numberOfFilms){
+    console.log("Вы киноман");
+}
